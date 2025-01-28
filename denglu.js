b4869358292291cd009c0ed3f488cd4d8d601029
追吻辰星 <1837601513@qq.com>
2025-01-29 00:01:04 +0800

    Update denglu.js

--- denglu.js
+++ denglu.js
@@ -10,6 +10,6 @@ $('#login-button').click(function (event) {
             location.href = "https://lcy20.netlify.app/gd.html";
         }, 1000);
     } else {
-        alert("用户名或密码不正确\n\n如登陆账号和密码忘记或多次错误❌请联系开发者获取账号密码\n\n联系邮件:  1837601513@qq.com！");
+        alert("\n用户名或密码不正确\n如登陆账号和密码忘记或多次错误❌请联系开发者获取账号密码\n\n联系邮件:  1837601513@qq.com！");
     }
 });
