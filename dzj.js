845ed441b27ae8e7a9f5347b627ed4dfabf02021
追吻辰星 <1837601513@qq.com>
2025-03-24 21:53:12 +0800

    Update dzj.js

--- dzj.js
+++ dzj.js
@@ -1,4 +1,4 @@
-  const text = "世界上的好东西都是抢来的 只有弱者才会坐等分配。 一利弗莫尔（黑色传奇）！";
+  const text = "世界上的好东西都是抢来的 只有弱者才会坐等分配，也许终有一天我会静下来， 像个局外人一样看着自己的故事！";
     const typingEffect = document.getElementById('typingEffect');
     let index = 0;
 
