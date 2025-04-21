12ee97cb632b55a563d5611923d800e43972ce28
追吻辰星 <1837601513@qq.com>
2025-04-21 09:33:09 +0800

    Update denglubkzc.js

--- denglubkzc.js
+++ denglubkzc.js
@@ -11,7 +11,7 @@ let users = [
         const foundUser = users.find(user => user.username === username && user.password === password);
         if (foundUser) {
             // 登录成功后，使用window.location.href进行页面跳转，这里跳转到success.html页面，可按需修改
-            window.location.href = "https://lcy20.netlify.app/yxc.html";
+            window.location.href = "https://lcy20.netlify.app/gd.html";
         } else {
             errorElement.textContent = '账号或密码错误，请重新输入，临时账号只可使用一次，若要永久账号请扫码联系管理员！';
         }
