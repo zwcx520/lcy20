96ba2e9c294d9ede40efc0b6cb7d5c9eae5b5166
追吻辰星 <1837601513@qq.com>
2025-05-01 21:04:36 +0800

    Add files via upload

---
+++ yyts.js
@@ -0,0 +1,8 @@
+    // 通过id获取页面上已存在的div元素
+    const targetDiv = document.getElementById('targetDiv');
+    if (targetDiv) {
+      // 创建一个文本节点
+      const textNode = document.createTextNode('背景音乐:一路生花');
+      // 将文本节点添加到div中
+      targetDiv.appendChild(textNode);
+    }
