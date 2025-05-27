1f26354381d73ff7824a9195747f45e847947401
追吻辰星 <1837601513@qq.com>
2025-05-27 23:43:28 +0800

    Update yyts.js

--- yyts.js
+++ yyts.js
@@ -2,7 +2,7 @@
     const targetDiv = document.getElementById('targetDiv');
     if (targetDiv) {
       // 创建一个文本节点
-      const textNode = document.createTextNode('背景音乐 : 九垓-毛亮&萨楚拉');
+      const textNode = document.createTextNode('背景音乐 : 汪苏泷-致曾来过的你');
       // 将文本节点添加到div中
       targetDiv.appendChild(textNode);
     }
