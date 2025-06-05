da37c1777afd470799066894d448b6054cce968c
追吻辰星 <1837601513@qq.com>
2025-06-06 06:17:30 +0800

    Update yyts.js

--- yyts.js
+++ yyts.js
@@ -2,7 +2,7 @@
     const targetDiv = document.getElementById('targetDiv');
     if (targetDiv) {
       // 创建一个文本节点
-      const textNode = document.createTextNode('背景音乐 : 一步一念 - 尹昔眠');
+      const textNode = document.createTextNode('背景音乐 : 月亮照山川 - 窝窝');
       // 将文本节点添加到div中
       targetDiv.appendChild(textNode);
     }
