a94d6db1377891529113d0b71e5159baf3c23ff9
追吻辰星 <1837601513@qq.com>
2025-06-22 10:32:02 +0800

    Update title.js

--- title.js
+++ title.js
@@ -2,7 +2,7 @@
     const targetTitle = document.getElementById('targetTitle');
     if (targetTitle) {
       // 创建一个文本节点
-      const textNode = document.createTextNode('故事不长，也不难讲，短短一句，相识一场！ᖰ˃̶ ꇴ ˂̶ᖳ');
+      const textNode = document.createTextNode('故事不长，也不难讲，短短一句，相识一场！ᖰ˃̶ ꇴ ˂̶ᖳ      ');
       // 将文本节点添加到div中
       targetTitle.appendChild(textNode);
     }
