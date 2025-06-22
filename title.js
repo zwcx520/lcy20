653886b5b81ae9d4e4406f30dc7984f67334dad5
追吻辰星 <1837601513@qq.com>
2025-06-22 10:28:18 +0800

    Create title.js

---
+++ title.js
@@ -0,0 +1,8 @@
+    // 通过id获取页面上已存在的div元素
+    const targetTitle = document.getElementById('targetTitle');
+    if (targetTitle) {
+      // 创建一个文本节点
+      const textNode = document.createTextNode('故事不长，也不难讲，短短一句，相识一场！');
+      // 将文本节点添加到div中
+      targetTitle.appendChild(textNode);
+    }
