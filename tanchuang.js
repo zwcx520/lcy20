41a99334bb6c06d07b2756dce698377edf68cb06
追吻辰星 <1837601513@qq.com>
2025-07-21 03:28:39 +0800

    Update tanchuang.js

--- tanchuang.js
+++ tanchuang.js
@@ -1,4 +1,4 @@
-/*
+/**/
 // 弹窗内容
 var popupNotice = "⭐归档提示⭐";
 var popupMainContent = "本APP和电脑版的UI已着重重新规划设计，APK与EXE安装包UI已同步更新，暂时无ios版本\n下载页重新布局设计，现已发布新版1.3版，电脑版同步优化，可以点击查看更多公告选择新版本体验！";
@@ -67,4 +67,4 @@ popupJoinButton.addEventListener("click", function(e) {
 
 // 显示弹窗
 showPopup();
-*/
+
