7aa8abd8140ba398c9baa3e230c2a4e2a4dcfe96
追吻辰星 <1837601513@qq.com>
2025-09-12 07:07:02 +0800

    Update title.js

--- title.js
+++ title.js
@@ -2,7 +2,7 @@
     const targetTitle = document.getElementById('targetTitle');
     if (targetTitle) {
       // 创建一个文本节点
-      const textNode = document.createTextNode('不要奢望能够赢得所有人的好感，因为那几乎 是不可能的的事情；按照平常自己待人接物的方法去接待其他人！ᖰ˃̶ ꇴ ˂̶ᖳ      ');
+      const textNode = document.createTextNode('不要奢望能够赢得所有人的好感，因为那几乎是不可能的的事情；按照平常自己待人接物的处理方式对待你遇到的每一个人就足够了！ᖰ˃̶ ꇴ ˂̶ᖳ      ');
       // 将文本节点添加到div中
       targetTitle.appendChild(textNode);
     }
