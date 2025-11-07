f10c3f80e4811f53c6312dcf88c4691f4c432d80
追吻辰星 <1837601513@qq.com>
2025-11-07 09:10:48 +0800

    Delete yyts.js

--- yyts.js
+++
@@ -1,8 +0,0 @@
-    // 通过id获取页面上已存在的div元素
-    const targetDiv = document.getElementById('targetDiv');
-    if (targetDiv) {
-      // 创建一个文本节点
-      const textNode = document.createTextNode('背景音乐 : 月亮照山川 - 窝窝');
-      // 将文本节点添加到div中
-      targetDiv.appendChild(textNode);
-    }
