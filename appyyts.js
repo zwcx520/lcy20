64e515ec56aa08b8b86788431e6e7023ba197f33
追吻辰星 <1837601513@qq.com>
2026-01-11 14:12:54 +0800

    Add files via upload

---
+++ appyyts.js
@@ -0,0 +1,5 @@
+
+        // 获取标签元素
+        const songTitle = document.querySelector('.song-title');
+        // 插入文字内容
+        songTitle.innerText = 'Let ME Konw';
---
+++ webyyts.js
@@ -0,0 +1,5 @@
+
+        // 获取标签元素
+        const songTitle = document.querySelector('.song-title');
+        // 插入文字内容
+        songTitle.innerText = '你存在我第二个不存在的世界 \n不见桃花不见秋';
