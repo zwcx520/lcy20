1582293fddf294df5c0be2d873576f646f42c641
追吻辰星 <1837601513@qq.com>
2026-03-02 13:11:13 +0800

    Update appyyts.js

--- appyyts.js
+++ appyyts.js
@@ -2,4 +2,4 @@
         // 获取标签元素
         const songTitle = document.querySelector('.song-title');
         // 插入文字内容
-        songTitle.innerText = 'Let ME Konw';
+        songTitle.innerText = '不如忘掉你的爱';
